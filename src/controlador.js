db21dc7db8abcb375ae7100259068575b052ddb9
dianaospina <dianapatricia@gmail.com>
2023-04-27 11:35:45 -0500

    Se agrega funcion buscar cancion

--- src/controlador.js
+++ src/controlador.js
@@ -6,7 +6,10 @@ let token=pedirToken().then(function(token) {
     console.log(token);
 
     buscarCancion(token).then(function(canciones){
-        console.log(canciones.tracks);
+        console.log(canciones.tracks[0]);
+        console.log(canciones.tracks[0].name);
+        console.log(canciones.tracks[0].preview_url);
+        console.log(canciones.tracks[0].album.images[0].url);
     })
 })
 
