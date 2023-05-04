edc7058f3a767034bac960b0ff530afa4fc9990c
dianaospina <dianapatricia@gmail.com>
2023-05-04 12:23:19 -0500

    ok

--- src/controlador.js
+++ src/controlador.js
@@ -1,30 +1,44 @@
 import { pedirToken } from "../services/servicioSpotify.js";
 import { buscarCancion } from "../services/servicioConsultarCasnciones.js";
 
-let fila=document.querySelector("#fila")
+let fila = document.querySelector("#fila")
+console.log(fila)
 
-let token=pedirToken().then(function(token) {
-    console.log(token);
+pedirToken().then(function(token) {
 
     buscarCancion(token).then(function(respuesta){
 
+            
             respuesta.tracks.forEach(function(cancion){
+
+                
                 let columna = document.createElement("div")
                 columna.classList.add("col")
 
                 let tarjeta = document.createElement("div")
                 tarjeta.classList.add("card","h-100","shadow")
+
+                let pista=document.createElement("audio")
+                pista.classList.add("w-100")
+                pista.setAttribute("controls","controls")
+                pista.src=cancion.preview_url
+
+                let foto = document.createElement("img")
+                foto.classList.add("img-fluid", "w-100")
+                foto.src=cancion.album.images[0].url
+
+                let nombre = document.createElement("h4")
+                nombre.classList.add("text-center", "fw-bold")
+                nombre.textContent=cancion.name
+
+                console.log(tarjeta)
+
+                tarjeta.appendChild(foto)
+                tarjeta.appendChild(nombre)
+                tarjeta.appendChild(pista)
+                columna.appendChild(tarjeta)
+                fila.appendChild(columna)
             })
-
-            let pista = document.createElement("audio")
-            pista.classList.add("w-100")
-            pista.setAttribute("controls", "controls")
-            pista.src=cancion.preview_url
-
-            tarjeta.appenchild(pista)
-            columna.appendChild(tarjeta)
-            fila.appendChild(columna)
-
         console.log(respuesta.tracks[0]);
         console.log(respuesta.tracks[0].name);
         console.log(respuesta.tracks[0].preview_url);
