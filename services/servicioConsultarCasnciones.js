3f467e23df628037accbf5296ecb9db50d47ba30
405 <405@academicos.cesde.edu.co>
2023-05-11 12:52:24 -0500

    subir el trabajo

--- services/servicioConsultarCasnciones.js
+++ services/servicioConsultarCasnciones.js
@@ -1,6 +1,6 @@
 
 export const buscarCancion = async(token) => {
-    const URL = "https://api.spotify.com/v1/artists/3ygJTpJJIK7eEeC2EFRl9D/top-tracks?market=US"
+    const URL = "https://api.spotify.com/v1/artists/2jSGzJw0ebJLu7OLVSOcBP/top-tracks?market=US"
 
     const PETICION ={
         method: 'GET',
